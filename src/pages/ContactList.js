6d6ad4a1761e19af972426fbec3cf33ba07c7f81
Anubarakh <30570291+Anubarakh@users.noreply.github.com>
2017-08-01 01:33:16 +0800

    Commit Message

--- src/pages/ContactList.js
+++ src/pages/ContactList.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {ListView, StyleSheet, Text} from 'react-native';
 export default class ContactList extends React.Component {
     render() {
-        return <Text>Hello ,this is contact list</Text>
+        return <Text>我是s+1，数学都是A</Text>
     }
 }
